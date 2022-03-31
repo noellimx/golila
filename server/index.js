75e2265e8e572280f35a3907eae8bf2eecf3441b
Noel Lim <noellimx@gmail.com>
2022-04-01 04:08:32 +0800

    server able to retrieve cookie from http request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import bindRoutes from "./routes/routes.js";
 
 const SERVER_LISTENING_PORT = 3004;
 const app = express();
 
 app.use(express.static("dist"));
+app.use(cookieParser());
+
 bindRoutes(app);
 
 app.listen(SERVER_LISTENING_PORT, () => {
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,8 @@
 import { resolve } from "path";
 
 export default (app) => {
-  app.get("/", (_, res) => {
-    console.log();
+  app.get("/", (req, res) => {
+    console.log(req.cookies)
     res.cookie("donotuse", "hehehehehe");
     res.sendFile(resolve("dist", "main.html"));
   });
