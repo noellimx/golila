fdb213bfb95de242ebbc267db40d13832de83060
Noel Lim <noellimx@gmail.com>
2022-04-01 02:06:10 +0800

    first route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,9 @@
 import { resolve } from "path";
 
-export default function bindRoutes(app) {
-  app.get("/", (request, response) => {
+export default (app) => {
+  app.get("/", (_, res) => {
     console.log(resolve("dist", "main.html"));
-    response.send("a");
+    res.cookie("donotuse", "hehehehehe");
+    res.send("a");
   });
-}
+};
