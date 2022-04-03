2f148c10922ee730c200edaa4484ec1a4a834581
Noel Lim <noellimx@gmail.com>
2022-04-03 20:20:10 +0800

    refine reseed

--- seed-db.js
+++ seed-db.js
@@ -6,18 +6,26 @@ const { user: User, room: Room } = sequelize.models;
 import { getUserByUsername } from "./server/database/api/user.js";
 try {
   // remove all records
-  const s = await User.truncate({ cascade: true });
-  const ss = await User.create({
+   await User.truncate({ cascade: true });
+  await Room.truncate({ cascade: true });
+
+
+  const user1Created = await User.create({
     username: "user",
     password: hashPassword("user"),
   });
 
-  const sss = await getUserByUsername("user");
+  const user1Retrieved = await getUserByUsername("user");
+
+  if (user1Retrieved.getDataValue("id") !== user1Created.getDataValue("id")){
+    throw new Error("[seed] user mismatch")
+  }
 
   await Room.create({
     name: "room-abc",
-    creatorId: sss.getDataValue("id"),
+    creatorId: user1Created.getDataValue("id"),
   });
+
 } catch (err) {
   console.log(err);
   throw err;
