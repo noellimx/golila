8e352e9fedc62866ffff8f8ff7db4eeff10a7a4f
Noel Lim <noellimx@gmail.com>
2022-04-05 08:29:38 +0800

    new migration create table gameplay

---
+++ server/database/models/init/gameplay.js
@@ -0,0 +1,64 @@
+import { Sequelize } from "sequelize";
+
+/**
+ * @param {Sequelize} sequelize
+ */
+export default (sequelize) => {
+  const { DataTypes } = sequelize.Sequelize;
+
+  const { room: Room } = sequelize.models;
+  if (!Room) {
+    throw new Error(
+      `[model] [${MODEL_NAME}] Are you sure there's valid model references? ${Room}`
+    );
+  }
+
+  const model = sequelize.define(
+    "gameplay",
+    {
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: "created_at",
+
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: "updated_at",
+
+      },
+
+      // ADD ATTRIBUTES HERE
+      roomId: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        references: {
+          model: Room,
+          key: "id",
+        },
+        allowNull: false,
+        field: "room_id",
+
+      },
+      chain: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "chain",
+      },
+      endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        field: "end_date",
+      }
+    },
+    {
+      underscored: true,
+    }
+  );
+  if (model !== sequelize.models.gameplay) {
+    throw new Error("model reference mismatch");
+  }
+
+  return model;
+};
--- server/database/models/init/index.js
+++ server/database/models/init/index.js
@@ -2,12 +2,13 @@ import initModelUser from "./user.js";
 import initModelRoom from "./room.js";
 import initModelParticipant from "./participant.js";
 import initModelLastKnownSessionUser from "./lastKnownSessionUser.js";
+import initModeGameplay from "./gameplay.js";
 const initModels = (db) => {
   const User = initModelUser(db);
   const Room = initModelRoom(db);
   const Participant = initModelParticipant(db);
   const LastKnowSessionUser = initModelLastKnownSessionUser(db);
-
+  const Gameplay = initModeGameplay(db)
   Participant.belongsTo(User, { foreignKey: "participantId" });
 };
 
